6e96f180f622654f95d3562cdc08c3ef1d52bf24
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2019-04-19 14:11:18 -0500

    ReneAnthonyAJ properly passing test with the submit button clearing form state

--- src/App.js
+++ src/App.js
@@ -5,43 +5,60 @@ import LabelledInput from './LabelledInput.js';
 class App extends Component {
   constructor(props){
     super(props);
-    this.handleSubmit=this.handleSubmit.bind(this);
     this.input = React.createRef();
+    this.state = {
+      name1: "",
+      cardHolderName: "",
+      ssn: "",
+      merchantName: ""
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(event) {
-    event.preventDefault();
-    event.target.form.reset();
+    // event.preventDefault();
+    // event.target.form.reset();
+    console.log('We are here');
+    this.setState({name1: null});
   }
 
-  setName() {
+  getState() {
+    return this.state
+  }
 
+  setName = (a) => {
+    console.log('We are here');
+    this.setState({name1: a})
+    console.log('====>' + a);
+  }
+
+  setCardHolderName = (cardholderName) => {
+    this.setState({cardHolderName: cardholderName});
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <form onSubmit={this.handleSubmit}>
+          {/* <form onSubmit={this.handleSubmit}> */}
+          <form>
             <label>
               Cardholder Name:
-              <input onChange={event => this.setName(event.target.value)} type="text"/>
+              <input onChange={event => this.setCardHolderName(event.target.value)} type="text"/>
             </label>
-            {/*<LabelledInput labelName="Cardholder Name"/>*/}
             <br></br>
             <label>
               SSN:
               <input onChange={event => this.setName(event.target.value)} type="text"/>
             <br></br>
             </label>
-            {/*<LabelledInput labelName="SSN"/>*/}
             <label>
               Merchant Name:
               <input onChange={event => this.setName(event.target.value)} type="text"/>
             </label>
             <br></br>
-            {/*<LabelledInput labelName="Merchant Name"/>*/}
-            <button id="blah" type="submit">Submit</button>
+            <button onClick={this.handleSubmit} type="submit" id="blah"/>
           </form>
         </header>
       </div>
--- src/UserInput.test.js
+++ src/UserInput.test.js
@@ -7,18 +7,23 @@ import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("user input", () => {
-    it('should clear the form with the user presses submit', () => {
+    it('should clear the form when the user presses submit', () => {
         const input = mount(<App/>);
         let userInputs = input.find('input');
-        const submitButton = input.find('#blah')
+        const submitButton = input.find('button#blah');
         
-        userInputs.forEach((name) => {name.props().value = "foo"});
+        userInputs.forEach((name) => {name.simulate('change', {target: {value: 'foo'}})});
+        const a = input.instance()
+        console.log(a.getState());
 
+        expect(a.state.name1).toEqual('foo');
+        // userInputs.forEach(input => expect(input.props().value).toEqual('foo'));
+        // console.log(a.getState());
         submitButton.simulate('click');
 
-        input.update();
-        userInputs = input.find('input');
-        userInputs.forEach(input => expect(input.props().value).toBeUndefined())
+        // userInputs = input.find('input');
+        // userInputs.forEach(input => expect(input.props().value).toBeUndefined());
+        expect(a.state.name1).toBeNull();
         
     });
 });
